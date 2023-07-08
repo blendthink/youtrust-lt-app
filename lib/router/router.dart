262dfaaeb19066adf20df5bc9c5e481906e38ae8
blendthink <admin@blendthink.dev>
2023-07-09 00:09:18 +0900

    create router

---
+++ lib/router/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:youtrust_lt_app/slides/title_slide.dart';
+
+part 'router.g.dart';
+
+@riverpod
+GoRouter router(RouterRef ref) => GoRouter(
+      routes: $appRoutes,
+    );
+
+@TypedGoRoute<TitleRoute>(
+  path: '/',
+)
+class TitleRoute extends GoRouteData {
+  const TitleRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => const TitleSlide();
+}
---
+++ lib/router/router.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $titleRoute,
+    ];
+
+RouteBase get $titleRoute => GoRouteData.$route(
+      path: '/',
+      factory: $TitleRouteExtension._fromState,
+    );
+
+extension $TitleRouteExtension on TitleRoute {
+  static TitleRoute _fromState(GoRouterState state) => const TitleRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routerHash() => r'6e0e8cc72835418fa80604eaeec9f9445786c67b';
+
+/// See also [router].
+@ProviderFor(router)
+final routerProvider = AutoDisposeProvider<GoRouter>.internal(
+  router,
+  name: r'routerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
