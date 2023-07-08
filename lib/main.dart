249dccd6149e80092879da4cf38cbc94f83cdb04
blendthink <admin@blendthink.dev>
2023-07-09 01:38:14 +0900

    set window min size

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:window_size/window_size.dart';
 import 'package:youtrust_lt_app/app.dart';
 
 void main() {
+  if (!Platform.isMacOS) {
+    throw Exception('Supported platforms are MacOS only');
+  }
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const double width = 1280;
+  const double height = width / 16 * 9;
+  setWindowMinSize(const Size(width, height));
+
   runApp(
     const ProviderScope(
       child: App(),
