78db7628cf56b00a3c2d12de12753c92291e2600
blendthink <admin@blendthink.dev>
2023-07-08 23:17:32 +0900

    create TitleSlide, IntroductionSlide

---
+++ lib/slides/introduction_slide.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IntroductionSlide extends StatelessWidget {
+  const IntroductionSlide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/slides/title_slide.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TitleSlide extends StatelessWidget {
+  const TitleSlide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
