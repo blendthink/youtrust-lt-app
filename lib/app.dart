d55ff0ff94c130879affd577d45407e779bb0f2b
blendthink <admin@blendthink.dev>
2023-07-14 08:45:14 +0900

    apply app themes

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:youtrust_lt_app/router/router.dart';
 import 'package:youtrust_lt_app/shortcuts/app_shortcuts.dart';
+import 'package:youtrust_lt_app/theme/theme.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -15,6 +16,8 @@ class App extends ConsumerWidget {
       routerDelegate: router.routerDelegate,
       debugShowCheckedModeBanner: false,
       shortcuts: appShortcuts,
+      theme: lightTheme,
+      darkTheme: darkTheme,
     );
   }
 }
