6780bbbcd783c30050b43145e4ca771a4fb31deb
blendthink <admin@blendthink.dev>
2023-07-21 09:39:03 +0900

    add method random.glsl

--- shaders/utils/random.glsl
+++ shaders/utils/random.glsl
@@ -1,3 +1,7 @@
+float random(in float st) {
+    return fract(sin(st) * 43758.5453123);
+}
+
 float random(in vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
