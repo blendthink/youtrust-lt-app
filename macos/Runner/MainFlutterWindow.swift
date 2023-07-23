1ce779115b508d2be45e64ba9071d595457ca2bf
blendthink <admin@blendthink.dev>
2023-07-23 13:08:07 +0900

    set initial content size

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,6 +7,7 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.setContentSize(NSSize(width: 640, height: 360))
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
